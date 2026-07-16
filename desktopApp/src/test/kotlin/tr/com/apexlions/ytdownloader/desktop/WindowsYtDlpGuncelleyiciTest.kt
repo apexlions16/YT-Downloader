83758ebe4efc1704ac2eb4b0aab528a166aabe49
apexlions16 <hcywashere@gmail.com>
2026-07-16 23:08:01 +0300

    Windows motor testini yeni kurucuya uyarla

--- desktopApp/src/test/kotlin/tr/com/apexlions/ytdownloader/desktop/WindowsYtDlpGuncelleyiciTest.kt
+++ desktopApp/src/test/kotlin/tr/com/apexlions/ytdownloader/desktop/WindowsYtDlpGuncelleyiciTest.kt
@@ -1,16 +1,14 @@
 package tr.com.apexlions.ytdownloader.desktop
 
-import kotlin.io.path.createTempDirectory
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class WindowsYtDlpGuncelleyiciTest {
+class WindowsMotorKurucusuTest {
     @Test
-    fun resmiToplamListesindenWindowsOzetiniAyiklar() {
-        val beklenen = "a".repeat(64)
-        val liste = "${"b".repeat(64)}  yt-dlp\n$beklenen  yt-dlp.exe\n"
-        val guncelleyici = WindowsYtDlpGuncelleyici(createTempDirectory())
-
-        assertEquals(beklenen, guncelleyici.beklenenSha256(liste, "yt-dlp.exe"))
+    fun varsayilanMotorDiziniWindowsYoluylaBiter() {
+        val yol = WindowsMotorKurucusu.varsayilanMotorDizini()
+        assertEquals("motor", yol.fileName.toString())
+        assertTrue(yol.toString().contains("YT İndirici"))
     }
 }
