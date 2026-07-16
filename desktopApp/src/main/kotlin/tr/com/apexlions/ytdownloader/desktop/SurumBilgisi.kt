94e77d3a9aa577186439100d3b686575097984ad
apexlions16 <hcywashere@gmail.com>
2026-07-16 23:53:40 +0300

    BPC varyantını paket JVM özelliğinden oku

--- desktopApp/src/main/kotlin/tr/com/apexlions/ytdownloader/desktop/SurumBilgisi.kt
+++ desktopApp/src/main/kotlin/tr/com/apexlions/ytdownloader/desktop/SurumBilgisi.kt
@@ -1,14 +1,8 @@
 package tr.com.apexlions.ytdownloader.desktop
 
 object SurumBilgisi {
-    private val ozellikler by lazy {
-        java.util.Properties().apply {
-            SurumBilgisi::class.java.getResourceAsStream("/surum.properties")?.use(::load)
-        }
-    }
-
     val developerSurumu: Boolean
-        get() = ozellikler.getProperty("developerSurumu", "false").toBoolean()
+        get() = System.getProperty("bpc.developerSurumu", "false").toBoolean()
 
     val uygulamaAdi: String
         get() = if (developerSurumu) "BPC Developer" else "BPC"
